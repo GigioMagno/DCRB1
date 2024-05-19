17f3f8667922ab375761fb30b5f99abe9de5d0cd
Svitol <97558303+GigioMagno@users.noreply.github.com>
2024-05-19 16:41:34 +0200

    Update bulk_db_batch.py

--- Code/bulk_db_batch.py
+++ Code/bulk_db_batch.py
@@ -37,7 +37,7 @@ def connect_db(host, user, pwd, db, auth):
 
 ########################################### SQL DDL #################################################
 ############################## Definition and creation of the tables ################################
-# Drop of the constraint on foreign key, drop of the tables if they exist yet
+# Drop of the tables if they exist yet
 # Setting of the charset for each table
 # Commit
 #####################################################################################################
